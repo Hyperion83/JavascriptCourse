8dd04b0aa2f33262e550d661415fab1d7d733dd1
Hyperion83 <arthas83@hotmail.com>
2018-06-26 16:14:08 +0100

    Init commit2

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,18 @@
+var markMass = prompt('How is Mark Mass?');
+var markHeight = prompt('How is Mark Height?');
+
+var johnMass = prompt('How is John Mass?');
+var johnHeight = prompt('How is John Height?');
+
+var markBMI = markMass / (markHeight * markHeight) ;  
+var johnBMI = johnMass / (johnHeight * johnHeight) ;  
+
+console.log('markMass:' + markBMI);
+console.log('johnMass:' + johnBMI);
+
+var winner = 'Mark';
+if(johnBMI > markBMI){
+    winner = 'John'
+}
+
+console.log('The winner is '+winner);
